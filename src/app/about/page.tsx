4a26b534db99cd4c962168519b71cb7bb63cf782
F.R Esa <goodquote39@hotmail.com>
2024-08-11 10:48:59 +0600

    Added new Choose component to AboutPage, including layout, text, and images.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -56,11 +56,69 @@ const AboutUs = () => {
   );
 };
 
+const Choose = () => {
+  return (
+    <div className="py-[190px] px-[260px] bg-[#F9F8F8]">
+      <div className="flex justify-between gap-12">
+        <div className="w-1/2">
+          <span className="tag">Why Choose us?</span>
+          <h1 className="text-5xl font-extrabold text-[50px] leading-tight mt-3">
+            We do not buy from the open market & traders.
+          </h1>
+          <p className="mt-2">
+            Simply dummy text of the printing and typesetting industry. Lorem
+            had ceased to been the industry's standard the 1500s, when an
+            unknown
+          </p>
+          <div className="mt-9 flex flex-col gap-6">
+            <div>
+              <div className="bg-[#ECECEC] flex items-center gap-2 p-8 w-[340px] rounded-full">
+                <img
+                  src="/Images/Icon.png"
+                  alt=""
+                  className="w-6 h-6"
+                />
+                <p className="font-semibold text-xl">100% Natural Product</p>
+              </div>
+              <p className="text-lg ms-14 mt-3 text-[#525C60]">
+                Simply dummy text of the printing and typesetting industry Lorem
+                Ipsum
+              </p>
+            </div>
+            <div>
+              <div className="bg-[#ECECEC] flex items-center gap-2 p-8 w-[340px] rounded-full">
+                <img
+                  src="/Images/Icon.png"
+                  alt=""
+                  className="w-6 h-6"
+                />
+                <p className="font-semibold text-xl">Increases resistance</p>
+              </div>
+              <p className="text-lg ms-14 mt-3 text-[#525C60]">
+                Filling, and temptingly healthy, our Biona Organic Granola with
+                Wild Berries is just the thing
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img
+            src="/Images/about/choose.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="mt-[90px] mx-[140px]">grid things</div>
+    </div>
+  );
+};
+
 const AboutPage = () => {
   return (
     <div>
       <Banner />
       <AboutUs />
+      <Choose />
     </div>
   );
 };
