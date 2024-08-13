4738631b16b6e24242796b9b0fc3d3408c4dec80
F.R Esa <goodquote39@hotmail.com>
2024-08-13 10:39:34 +0600

    Added PortfolioList component and imported necessary dependencies

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import Subscribe from "@/components/Subscribe";
+import { portfolioLists } from "@/contents/portfolioList";
 import React from "react";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Banner = () => {
   return (
@@ -18,10 +20,39 @@ const Banner = () => {
   );
 };
 
+const PortfolioList: React.FC = () => {
+  return (
+    <div className="mt-[150px] px-[260px] grid grid-cols-3 gap-x-6 gap-y-11">
+      {portfolioLists.map((item) => (
+        <div
+          key={item.id}
+          className="group"
+        >
+          <div className="rounded-2xl overflow-hidden relative ">
+            <img
+              className="w-full"
+              src={item.image}
+              alt=""
+            />
+            <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-70 m-6 rounded-2xl scale-0 group-hover:scale-100 transition-scale duration-300">
+              <p className="text-[24px] font-bold bg-[#7eb693] rounded-full p-3">
+                <MdKeyboardArrowRight className="text-white" />
+              </p>
+            </div>
+          </div>
+          <h4 className="mt-5 text-2xl font-semibold">{item.title}</h4>
+          <span className="tag-two">{item.type}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 const Portfolio = () => {
   return (
     <div>
       <Banner />
+      <PortfolioList />
       <div className="my-[140px] w-full">
         <Subscribe />
       </div>
